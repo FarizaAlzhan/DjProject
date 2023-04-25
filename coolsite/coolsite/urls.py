b8d564408320b72616d916310d8aec332f6192b5
Asemaaaaaaaaaa <assem.syrymbetova@narxoz.kz>
2023-04-25 11:08:01 +0600

    Ex12 1. Улучшите свой CRUD с помощью ModelViewSet и роутеров

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -71,8 +71,66 @@ if settings.DEBUG:
    ] + urlpatterns
    urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
+from django.urls import path,include
+
+from rest_framework import routers
+
+class MyCustomRouter(routers.SimpleRouter):
+    routes = [
+        routers.Route(url=r'^{prefix}$',
+                      mapping={'get':'list'},
+                      name='{basename}-list',
+                      detail=False,
+                      initkwargs={'suffix':'List'}),
+        routers.Route(url=r'{prefix}/{lookup}$',
+                      mapping={'get':'retrieve'},
+                      name='{basename}-detail',
+                      detail=True,
+                      initkwargs={'suffix':'Detail'})
+    ]
+# class MyCustomRouter(routers.SimpleRouter):
+#     routes = [
+#         routers.Route(url=r'^{prefix}$',
+#                       mapping={'get':'list'},
+#                       name='{basename}-list',
+#                       detail=False,
+#                       initkwargs={'suffix':'List'}),
+#         routers.Route(url=r'{prefix}/{lookup}$',
+#                       mapping={'get':'retrieve'},
+#                       name='{basename}-detail',
+#                       detail=True,
+#                       initkwargs={'suffix':'Detail'})
+#     ]
+router = routers.SimpleRouter()
+router.register(r'book',BookViewSet)
+# router.register(r'book',BookViewSet)
+router.register(r'training1',Training1ViewSet)
+router.register(r'training2',Training2ViewSet)
+router.register(r'training_manager1',Training_manager1ViewSet)
+@@ -47,16 +46,13 @@class MyCustomRouter(routers.SimpleRouter):
+    path('admin/', admin.site.urls),
+    path('captcha/', include('captcha.urls')),
+    path('',include('project.urls')),
+    path('api/v1/',include(router.urls)),#http://127.0.0.1:8000/api/v1/training1/
+
+    path('api/v1/book/',BookAPIList.as_view()),
+    path('api/v1/bookcreate/',BookAPICreate.as_view()),
+    path('api/v1/book/<int:pk>/',BookAPIUpdate.as_view()),
+    path('api/v1/bookdelete/<int:pk>/',BookAPIDestroy.as_view()),
+
+    path('api/v1/',include(router.urls)),#http://127.0.0.1:8000/api/v1/book/
+
+    # path('api/v1/booklist/',BookViewSet.as_view({'get': 'list'})),
+    # path('api/v1/booklist/<int:pk>/',BookViewSet.as_view({'put': 'update'})),
+    # path('api/v1/bookdetail/<int:pk>/',BookAPIDetailView.as_view()),
+
+    # path('api/v1/training1list/',Training1APIView.as_view()),
+    # path('api/v1/training2list/',Training2APIView.as_view())
+
+
 
 handler404 = pageNotFound
 handler403 = forbidden
 handler500 = serverError
 handler400 = badRequest
+]
